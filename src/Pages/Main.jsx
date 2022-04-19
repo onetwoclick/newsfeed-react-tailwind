b242af894d95ff2336789f46cc0be3942a2d6d58
onetwoclick <i.ivanov@internet.ru>
2022-04-19 22:37:14 +0300

    left sidebar

--- src/Pages/Main.jsx
+++ src/Pages/Main.jsx
@@ -5,6 +5,40 @@ function classNames(...classes) {
   return classes.filter(Boolean).join(' ')
 }
 
+const posts = [
+  {
+    title: 'Boost your conversion rate',
+    href: '#',
+    description:
+      'Illo sint voluptas. Error voluptates culpa eligendi. Hic vel totam vitae illo. Non aliquid explicabo necessitatibus unde. Sed exercitationem placeat consectetur nulla deserunt vel. Iusto corrupti dicta.',
+    date: 'Mar 16, 2020',
+    datetime: '2020-03-16',
+  },
+  {
+    title: 'How to use search engine optimization to drive sales',
+    href: '#',
+    description: 'Optio cum necessitatibus dolor voluptatum provident commodi et. Qui aperiam fugiat nemo cumque.',
+    date: 'Mar 10, 2020',
+    datetime: '2020-03-10',
+  },
+  {
+    title: 'Improve your customer experience',
+    href: '#',
+    description:
+      'Cupiditate maiores ullam eveniet adipisci in doloribus nulla minus. Voluptas iusto libero adipisci rem et corporis.',
+    date: 'Feb 12, 2020',
+    datetime: '2020-02-12',
+  },
+  {
+    title: 'Writing effective landing page copy',
+    href: '#',
+    description:
+      'Ipsum voluptates quia doloremque culpa qui eius. Id qui id officia molestias quaerat deleniti. Qui facere numquam autem libero quae cupiditate asperiores vitae cupiditate. Cumque id deleniti explicabo.',
+    date: 'Jan 29, 2020',
+    datetime: '2020-01-29',
+  },
+]
+
 export default function Example() {
   return (
     <>
@@ -39,8 +73,17 @@ export default function Example() {
           <div className="bg-gray-50 pr-4 sm:pr-6 lg:pr-8 lg:flex-shrink-0 lg:border-l lg:border-gray-200 xl:pr-0">
             <div className="h-full pl-6 py-6 lg:w-80">
               {/* Start right column area */}
-              <div className="h-full relative" style={{ minHeight: '16rem' }}>
-                <div className="absolute inset-0 border-2 border-gray-200 border-dashed rounded-lg" />
+              <div className="mt-2 pt-2 grid gap-4 lg:grid-cols-1 lg:gap-x-2 lg:gap-y-4">
+                {posts.map((post) => (
+                  <div key={post.title}>
+                    <p className="text-sm text-gray-500">
+                      <time dateTime={post.datetime}>{post.date}</time>
+                    </p>
+                    <a href="#" className="mt-2 block">
+                      <p className="text-xl font-semibold text-gray-900">{post.title}</p>
+                    </a>
+                  </div>
+                ))}
               </div>
               {/* End right column area */}
             </div>
